0d91096ed8d3e0a2795f3cde91599b313608c30f
Piyush Ingale <68149489+PiyushIngale007@users.noreply.github.com>
2021-02-13 21:13:13 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ function getResult(token) {
     {
       method: 'GET',
       headers: {
-        'x-rapidapi-key': 'c5e4f34e74msh98beac42f63fb9ep1d10bajsn99f8d127891d',
+        'x-rapidapi-key': '9a1e533b35msh3b49916d58c4124p1389c5jsnfc392a00200e',
         'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
       },
     }
@@ -44,7 +44,7 @@ function onClickRun() {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
-        'x-rapidapi-key': 'c5e4f34e74msh98beac42f63fb9ep1d10bajsn99f8d127891d',
+        'x-rapidapi-key': '9a1e533b35msh3b49916d58c4124p1389c5jsnfc392a00200e',
         'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
       },
       body: data,
@@ -58,3 +58,90 @@ function onClickRun() {
     });
 }
 
+function gettoken() {
+  document.getElementById('console').innerHTML = '';
+  console.log(encode($('#source').val()));
+  var data = JSON.stringify({
+    language_id: 71,
+    source_code: encode($('#source').val()),
+    stdin: encode($('#input').val()),
+  });
+
+  console.log(data);
+  $('#loading').css('display', 'unset');
+  fetch(
+    'https://judge0-ce.p.rapidapi.com/submissions?base64_encoded=true&fields=*',
+    {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'x-rapidapi-key': '9a1e533b35msh3b49916d58c4124p1389c5jsnfc392a00200e',
+        'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
+      },
+      body: data,
+    }
+  )
+    .then((response) => response.text())
+    .then((token) => {
+      setTimeout(function () {
+        getResult1(JSON.parse(token));
+      }, 1000);
+    });
+}
+
+function getResult1(token) {
+  console.log(token.token);
+  fetch(
+    'https://judge0-ce.p.rapidapi.com/submissions/' +
+      token.token +
+      '?base64_encoded=false&fields=*',
+    {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-key': '9a1e533b35msh3b49916d58c4124p1389c5jsnfc392a00200e',
+        'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      $('#loading').css('display', 'none');
+      if (data.stdout == null) {
+        document.getElementById('console').innerHTML = data.stderr;
+      } else {
+        document.getElementById('console').innerHTML = data.stdout;
+      }
+    });
+}
+
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById('console');
+var captionText = document.getElementById('caption');
+img.onclick = function () {
+  modal.style.display = 'unset';
+  modalImg.src = this.src;
+  gettoken();
+};
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onorientationchange = function () {
+  var orientation = window.orientation;
+  switch (orientation) {
+    case 0:
+    case 90:
+    case -90:
+      window.location.reload();
+      break;
+  }
+};
